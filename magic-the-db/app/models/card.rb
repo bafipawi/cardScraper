c483b942b1879becd18d6d7ecc6633fda60261ef
Jon Rose <jonrose08@gmail.com>
2013-03-25 15:53:21 -0700

    add typhosoifeoiens for parallel requests

--- magic-the-db/app/models/card.rb
+++ magic-the-db/app/models/card.rb
@@ -1,3 +1,6 @@
+
+
+require "typhoeus"
 require 'nokogiri'
 require 'open-uri'
 
@@ -9,21 +12,36 @@ class Card < ActiveRecord::Base
 
   def self.scrape_cards(max)
     list_url = "http://deckbox.org/games/mtg/cards?p="
+
     doc = Nokogiri::HTML(open(list_url + 1 .to_s))
+
     last_page_num = doc.search("div#set_cards_table div.controls div.pagination_controls a").last["href"].split("=").last.to_i
     max_num = max > last_page_num ? last_page_num : max
-    puts "max #{max_num}"
-    threads = []
-    urls = []
 
-    (1..max_num).each do |i|
-      url = list_url + i.to_s
-      dom = Nokogiri::HTML(open(url))
+    index_urls = (1..max_num).map { |i| list_url + i.to_s }
+    card_urls = []
 
-      anchors = dom.search("td.card_name a.simple")
-      urls << anchors.map{|a| a["href"]}
-
+    requests = index_urls.map{ |url| Typhoeus::Request.new(url)}
+    requests.map do |r|
+      r.on_complete do |response|
+        dom = Nokogiri::HTML(response.body)
+        anchors = dom.search("td.card_name a.simple").map{|a| a["href"]}
+        card_urls << anchors
+        anchors
+      end
     end
-    urls
+
+    hydra = Typhoeus::Hydra.new
+
+    for request in requests
+      hydra.queue(request)
+    end
+    hydra.run
+
+    card_urls
   end
 end
+
+reload!
+
+Card.scrape_cards(2)
